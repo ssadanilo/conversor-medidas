3e26f1f6c713eb5c33eabe44a738c38922ab35d8
Danilo <ssadanilo@hotmail.com>
2024-02-20 14:20:00 -0300

    conversorMedidas

---
+++ conversorMedidas.py
@@ -0,0 +1,15 @@
+numero_metro = int(input('Digite um número consideando que a medida dele é em metros: '))
+km = numero_metro * 0.001
+hm = numero_metro * 0.01
+dam = numero_metro * 0.1
+dm = numero_metro * 10
+cm = numero_metro * 100
+mm = numero_metro * 1000
+
+print(f'{numero_metro} m equivale a {km} km')
+print(f'{numero_metro} m equivale a {hm} hm')
+print(f'{numero_metro} m equivale a {dam} dam')
+print(f'{numero_metro} m equivale a {dm} dm')
+print(f'{numero_metro} m equivale a {cm} cm')
+print(f'{numero_metro }m equivale a {mm} mm')
+
